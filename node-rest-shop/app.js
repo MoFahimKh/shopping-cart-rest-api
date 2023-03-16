c1e97ee3da391543a1fff0b10c0575b26adbdc33
M Fahim KHAN <mkhan@qodeleaf.com>
2023-03-16 18:33:18 +0530

    Add feature for handle CORS errors

--- node-rest-shop/app.js
+++ node-rest-shop/app.js
@@ -1,29 +1,44 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
-app.use(morgan('dev'));
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
-const productRoutes = require('./api/routes/products');
-
-const orderRoutes = require('./api/routes/orders');
-
-
-app.use('/products', productRoutes);
-app.use('/orders', orderRoutes);
+const productRoutes = require("./api/routes/products");
+app.use(morgan("dev"));
+//handeling CORS errors
+app.use((req, res, next) => {
+  //to give access to any client
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept",
+    "Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    //which request you want to allow
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+});
+const orderRoutes = require("./api/routes/orders");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
 
 //handeling errors
 app.use((req, res, next) => {
-    const error = new Error('Not Found');
-    error.status = 404;
-    next(error);
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
 });
 app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    res.json({
-        error:{
-            message : error.message
-        }
-    });
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
 });
 
 module.exports = app;
