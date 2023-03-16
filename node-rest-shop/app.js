24193e7a36d461d07d944947f6d207a55db9f3f1
M Fahim KHAN <mkhan@qodeleaf.com>
2023-03-16 18:13:47 +0530

    Add error handeling for client and server errors

--- node-rest-shop/app.js
+++ node-rest-shop/app.js
@@ -11,6 +11,19 @@ const orderRoutes = require('./api/routes/orders');
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
-
+//handeling errors
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error:{
+            message : error.message
+        }
+    });
+});
 
 module.exports = app;
