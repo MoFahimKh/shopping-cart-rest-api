bfd7ca1af854017cd7f32ee1e21b46cc6af2ca70
M Fahim KHAN <mkhan@qodeleaf.com>
2023-03-17 12:34:44 +0530

    Fix linting with air bnb style

--- node-rest-shop/api/routes/products.js
+++ node-rest-shop/api/routes/products.js
@@ -1,25 +1,28 @@
-const exress = require("express");
-const router = exress.Router();
-const Product = require("../models/product");
-const mongoose = require("mongoose");
+/* eslint-disable no-shadow */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-restricted-syntax */
+const express = require('express');
 
-router.get("/", (req, res, next) => {
+const router = express.Router();
+const mongoose = require('mongoose');
+const Product = require('../models/product');
+
+router.get('/', (req, res) => {
   Product.find()
     .exec()
     .then((docs) => {
       console.log(docs);
       if (docs.length >= 0) {
         res.status(200).json(docs);
-      } else {
-        res.status(404).json({ error: err });
       }
     })
     .catch((err) => {
       console.log(err);
+      res.status(500).json({ error: err });
     });
 });
 
-router.post("/", (req, res, next) => {
+router.post('/', (req, res, next) => {
   const product = new Product({
     _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
@@ -31,7 +34,7 @@ router.post("/", (req, res, next) => {
     .then((result) => {
       console.log(result);
       res.status(201).json({
-        message: "Product created successfully",
+        message: 'Product created successfully',
         createProduct: result,
       });
     })
@@ -41,14 +44,14 @@ router.post("/", (req, res, next) => {
     });
 });
 
-router.get("/:productId", (req, res, next) => {
+router.get('/:productId', (req, res, next) => {
   const id = req.params.productId;
   Product.findById(id)
     .exec()
     .then((doc) => {
-      console.log("from db", doc);
+      console.log('from db', doc);
       if (doc) res.status(200).json({ doc });
-      else res.status(404).json({ message: "Product not found" });
+      else res.status(404).json({ message: 'Product not found' });
     })
     .catch((err) => {
       console.log(err);
@@ -56,18 +59,18 @@ router.get("/:productId", (req, res, next) => {
     });
 });
 
-router.patch("/:productId", (req, res, next) => {
+router.patch('/:productId', (req, res, next) => {
   const updateOps = {};
   for (const ops of req.body) {
     updateOps[ops.propName] = ops.value;
   }
   Product.update({ _id: req.params.productId }, { $set: updateOps })
     .exec()
-    .then((res) => res.status(200).json({ message: "Product updated" }))
-    .catch(res.status(500).json({ error: err }));
+    .then((res) => res.status(200).json({ message: 'Product updated' }))
+    .catch((err) => res.status(500).json({ error: err }));
 });
 
-router.delete("/:productId", (req, res, next) => {
+router.delete('/:productId', (req, res, next) => {
   Product.remove({ _id: req.params.productId })
     .exec()
     .then((result) => {
--- node-rest-shop/app.js
+++ node-rest-shop/app.js
@@ -1,38 +1,40 @@
-const express = require("express");
+/* eslint-disable consistent-return */
+/* eslint-disable no-unused-vars */
+const express = require('express');
+
 const app = express();
-const morgan = require("morgan");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
-const productRoutes = require("./api/routes/products");
-const orderRoutes = require("./api/routes/orders");
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
 
-mongoose.connect('mongodb+srv://mkhan:'+process.env.mongodb_password+'@cluster0.4hxwplz.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connect(`mongodb+srv://mkhan:${process.env.mongodb_password}@cluster0.4hxwplz.mongodb.net/?retryWrites=true&w=majority`);
 
-app.use(morgan("dev"));
-//handeling CORS errors
+app.use(morgan('dev'));
+// handeling CORS errors
 app.use((req, res, next) => {
-  //to give access to any client
-  res.header("Access-Control-Allow-Origin", "*");
+  // to give access to any client
+  res.header('Access-Control-Allow-Origin', '*');
   res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept",
-    "Authorization"
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+    'Authorization',
   );
-  if (req.method === "OPTIONS") {
-    //which request you want to allow
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+  if (req.method === 'OPTIONS') {
+    // which request you want to allow
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
     return res.status(200).json({});
   }
 });
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use("/products", productRoutes);
-app.use("/orders", orderRoutes);
+app.use(express.bodyParser.urlencoded({ extended: false }));
+app.use(express.bodyParser.json());
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
 
-//handeling errors
+// handeling errors
 app.use((req, res, next) => {
-  const error = new Error("Not Found");
+  const error = new Error('Not Found');
   error.status = 404;
   next(error);
 });
